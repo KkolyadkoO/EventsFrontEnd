b01659993195af5b7086f941c128060d182b763e
KkolyadkoO <artem,7ko52@gmail.com>
2024-09-20 01:49:17 +0300

    Change button on view event

--- src/components/InformationAboutEvent/index.tsx
+++ src/components/InformationAboutEvent/index.tsx
@@ -8,11 +8,11 @@ type Props = {
 }
 
 
-const InformationOfEvent = ({ event, category }: Props) => {
+const InformationOfEvent = ({event, category}: Props) => {
     if (!event) {
         return <p>No event information available</p>;
     }
-    const { title, imageUrl, date, description, location, maxNumberOfMembers, numberOfMembers } = event;
+    const {title, imageUrl, date, description, location, maxNumberOfMembers, numberOfMembers} = event;
 
     const formatDate = (dateString: string): string => {
         const date = new Date(dateString);
@@ -26,18 +26,22 @@ const InformationOfEvent = ({ event, category }: Props) => {
     return (
         <div className="information-of-event">
             <span className="title">{title}</span>
-            <img className="image" src={imageUrl || "https://placehold.co/1280x720"} alt={title} />
+            <img className="image" src={imageUrl || "https://placehold.co/1280x720"} alt={title}/>
             <span className="description">{description}</span>
-                <div className="info">
-                    <span className="info-text">{formatDate(date)}</span>
-                    <span className="info-text">{numberOfMembers}/{maxNumberOfMembers}</span>
-                </div>
-                <div className="info">
-                    <span className="info-text">{location}</span>
-                    <span className="info-text">{category.title}</span>
+            <div className="info">
+                <span className="info-text">{formatDate(date)}</span>
+                <span className="info-text">{numberOfMembers}/{maxNumberOfMembers}</span>
             </div>
+            <div className="info">
+                <span className="info-text">{location}</span>
+                <span className="info-text">{category.title}</span>
+            </div>
+            {(numberOfMembers == maxNumberOfMembers) ? (
+                <button className="sing-up">Full</button>
+            ) : (
+                <button className="sing-up">Sing up</button>
+            )}
 
-            <button className="sing-up">Sing up</button>
         </div>
     )
 }
--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -28,7 +28,6 @@ const HomePage = () => {
             }
 
         }
-
         fetchEvents();
     }, []);
 
